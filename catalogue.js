4d03e600a9fc499233ee5d0f6405649edec51b58
Christiane <christal.rieux65@gmail.com>
2020-11-05 17:44:57 +0100

    boostrap + javascript du catalogue et page de garde

--- catalogue.js
+++ catalogue.js
@@ -2,24 +2,43 @@ const search = window.location.search
 
 const param = new URLSearchParams(search)
 const id = param.get("id")
-console.log(id);
+console.log();
 
-fetch("http://localhost:3000/api/teddies/" + id)
-.then(response => response.json())
-.then(response => {
-  console.log(response);
-  response.forEach(element => {
-    const div=document.createElement("div")
-    div.innerHTML = `
-    <div class="produit">
-     
-      <img src="${element.imageUrl}" alt="teddy_1" title="Norbert" height="300">
-      <h3 class="produitOursonTeddy">${element.name}</h3>
-      <a href="catalogue.html?id=${element._id}">Voir le produit</a>
-    </div>`
-  
-    container.appendChild(div)
-  });
-  })
-  .catch(error => alert("Erreur : " + error));
+
+
+  fetch("http://localhost:3000/api/teddies")
+  .then(response => response.json())
+  .then(data => {
+ 
+  for (i =0; i < data.length; i++){  
+    // Boucle for pour créer la liste des produits
+     article = newCreateItem (data[i]._id,data[i].name,data[i].imageUrl,data[i].description,data[i].price);
+     addElement(article._id,article.name,article.imageUrl,article.description,article.price);
+ };
+
+// Fonction de création d'un produit
+
+function newCreateItem (id,name,imageUrl,description,price){
+this._id = id,
+this.name = name,
+this.imageUrl = imageUrl,
+this.description = description,
+this.price = price
+
+
+// Ajout du produit au tableau 
+
+  let article = document.getElementById('article');
+ 
+article.dataset.name 
+article.dataset.imageUrl
+article.dataset.description
+article.dataset.price
+ 
+  article.appendChild(article)
+}
+})
+.catch(error => alert("Erreur : " + error));
+
+
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const container=document.getElementById("pages_garde")
 
 
@@ -8,12 +9,14 @@ fetch("http://localhost:3000/api/teddies")
 response.forEach(element => {
   const div=document.createElement("div")
   div.innerHTML = `
-  <div class="produit">
-  <div class="jumbotron">
-    <img src="${element.imageUrl}" alt="teddy_1" title="Norbert" height="300">
+  <div class="produit mx-auto shadow-lg">
+  <div class="jumbotron x-5">
+    <img src="${element.imageUrl}" class="img-responsive center-block mx-auto shadow-lg" alt="teddy_1" title="Norbert" height="300">
    
     <h3 class="produitOursonTeddy">${element.name}</h3>
    <a href="catalogue.html?id=${element._id}">Voir le produit</a>
+   <p class="description">${element.description}</p>
+   <p class="price">${element.price} euro</p>
   </div>`
 
   container.appendChild(div)
