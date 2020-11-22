8d810bbd12deb4ef0f6a64014ff5649bc37ae8dd
Christiane <christal.rieux65@gmail.com>
2020-11-22 18:06:29 +0100

    total commande panier js

--- panier.js
+++ panier.js
@@ -13,25 +13,33 @@ if (!panierContent || panierContent.length < 1){
         totalPrixCommande += element.price
         const tr = document.createElement("tr")
         const tdNom = document.createElement("td")
+        const tdColors = document.createElement("td")
         const tdImg = document.createElement("td")
         const tdPrix = document.createElement("td")
         tdNom.textContent = element.name
+        tdColors.innerHTML = `
+       <p>${element.colors}</p>`
         tdImg.innerHTML = `
         <img src="${element.imageUrl}"/>`
         tdImg.className = "imagePanier"
         tdPrix.innerHTML = `
-        <td  id="col-2" class="${element.price}>< euro"/>`
-        tdPrix.className = "prixteddy lead"
+        <td  id="col-2">
+        <div class="price lead"><strong>${element.price} euro</strong></div>`    
+        tdPrix.className = "prixteddy lead strong"
         tdPrix.textContent = element.price
         tr.appendChild(tdNom)
+        tr.appendChild(tdColors)
         tr.appendChild(tdImg)
         tr.appendChild(tdPrix)
         teddies.appendChild(tr)
-    });
+      });
+    // Calcul du total panier //
     const pTotal = document.createElement("p")
     pTotal.textContent = totalPrixCommande
     pTotal.className = "montantTotal lead mark"
+    pTotal.innerHTML = "<h4> Montant total de votre commande </h4>" + totalPrixCommande + " euro"
     container.appendChild(pTotal)   
+    
 }
 
 
