2fbf71719f964a0bc602d2041ba13ae479326cec
Christiane <christal.rieux65@gmail.com>
2020-12-13 09:49:55 +0100

    design

--- confirmation.js
+++ confirmation.js
@@ -6,3 +6,12 @@ const id = param.get("bonDeCommande")
 const total = param.get("total")
 document.getElementById("numero").textContent = id
 document.getElementById("montant").textContent = total
+montant.innerHTML =  total + " euro"
+numero.innerHTML = id + " a bien été enregistrée."
+
+
+// const pTotal = document.createElement("p")
+//     pTotal.textContent = totalPrixCommande
+//     pTotal.className = "montantTotal lead mark"
+//     pTotal.innerHTML = "<h4> Montant total de votre commande </h4>" + totalPrixCommande + " euro"
+//     container.appendChild(pTotal);
