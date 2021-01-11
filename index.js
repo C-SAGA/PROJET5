62baccf42b89abfd899fd2c9f57491ddba6f607d
Christiane <christal.rieux65@gmail.com>
2021-01-11 17:14:30 +0100

    mentions légales

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ fetch("http://localhost:3000/api/teddies")
 
  // Intégration code HTML via ``//
  div.innerHTML = ` 
-      <div class="liste_produit mx-2 px-2">
-         <div class="produit shadow-lg justify-content-center">
-            <h3 class="produitOursonTeddy text-left">${element.name}</h3>
-               <div class="jumbotron mx-4">
-                 <img src="${element.imageUrl}" class="img-responsive center-block " height="300"><br><br>
-                 <a class="btn btn-secondary" href="catalogue.html?id=${element._id}">Voir le produit</a>
-                 <p class="description pl-2 pl-2 text-justify lead">${element.description}</p>
-                 <p class="price lead"><strong><mark>${element.price} euro</mark></strong</p>
-                </div>
-          </div>
-       </div>`
+         <div class="liste_produit mx-2 px-2">
+            <div class="produit shadow-lg justify-content-center">
+                  <h3 class="produitOursonTeddy text-left">${element.name}</h3>
+                     <div class="jumbotron mx-4">
+                     <img src="${element.imageUrl}" class="img-responsive center-block " height="300"><br><br>
+                     <a class="btn btn-secondary" href="catalogue.html?id=${element._id}">Voir le produit</a>
+                     <p class="description pl-2 pl-2 text-justify lead">${element.description}</p>
+                     <p class="price lead"><strong><mark>${element.price} euro</mark></strong</p>
+                     </div>
+            </div>
+         </div>`
 
   container.appendChild(div) //ajouter des élements enfants (ici des fiches produit)//
  });
